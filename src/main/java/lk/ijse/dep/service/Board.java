9d6084705abc7593b0aa8439c6c5e3a2c94d54bf
Oshan shanuka <157113803+oshanshanuka@users.noreply.github.com>
2024-10-01 21:02:01 +0530

    first commit

---
+++ src/main/java/lk/ijse/dep/service/Board.java
@@ -0,0 +1,18 @@
+package lk.ijse.dep.service;
+
+public interface Board {
+    public int NUM_OF_ROWS = 5;
+    public int NUM_OF_COLS = 6;
+    BoardUI getBoardUI();
+    int findNextAvailableSpot(int col);
+
+    boolean isLegalMove(int col);
+
+    boolean existLegalMoves();
+
+    void updateMove(int col, Piece move);
+    void updateMove(int col, int row,Piece move);
+    Piece[][] getPieces();
+    Winner findWinner();
+
+}
